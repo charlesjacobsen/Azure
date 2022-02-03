9613c8188e6ab74be71b153ce2d9afbca3595e53
Charles Jacobsen <44608130+charlesjacobsen@users.noreply.github.com>
2022-02-03 09:46:22 -0300

    Delete IIS.ps1

--- IIS.ps1
+++
@@ -1,8 +0,0 @@
-# Install IIS 
-Install-WindowsFeature -name Web-Server -IncludeManagementTools 
-
-# Remove default htm file 
-remove-item C:\inetpub\wwwroot\iisstart.htm
-
-#Add custom htm file 
-Add-Content -Path "C:\inetpub\wwwroot\iisstart.htm" -Value $("TESTE IIS " + $env:computername)
